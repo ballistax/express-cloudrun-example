43222f0f515ad91502b426892bfef348b69d230f
Thakoon Narksrimuang <ball_150637@hotmail.com>
2021-02-11 15:41:56 +0700

    create ci/cd workflow

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const app = express()
+const port = process.env.PORT || 3000
 
 app.get('/', function (req, res) {
   res.json({message: "hello"})
 })
 
-app.listen(3000, () => {
-  console.log(`Example app listening at http://localhost:3000`)
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
 })
